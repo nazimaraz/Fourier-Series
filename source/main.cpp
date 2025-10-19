076099018fb01ac6cc119932cea17e0a434c1b7b
nazimaraz <46851561+nazimaraz@users.noreply.github.com>
2025-10-19 21:13:36 +0300

    Refactoring and sawtooth wave is implemented

--- source/main.cpp
+++ source/main.cpp
@@ -3,19 +3,22 @@
 //
 
 #include <boost/circular_buffer.hpp>
-#include <string>
 #include <raylib.h>
 #include <imgui.h>
 #include <rlImGui.h>
+#include "Function.h"
 #include "math/math.h"
 
 void Setup();
 void Update();
+std::pair<int, float> GetFormula(int i);
 
 auto t = 0.f;
 auto speed = 0.02f;
 auto wave = boost::circular_buffer<float>{1000};
 auto N = 7;
+auto current_function = Function::SQUARE_WAVE;
+auto constant_radius = 75.f;
 
 int main()
 {
@@ -51,9 +54,16 @@ void Update()
     ImGui::SetNextWindowPos({0.f, 0.f});
     ImGui::Begin("Main Content", nullptr, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoMove);
     ImGui::Text("FPS: %d", raylib::GetFPS());
+    if (ImGui::Button("Clear"))
+        wave.clear();
 
+    static auto const function_names = std::vector{"Square Wave", "Sawtooth Wave", "Triangle Wave", "Semicircle"};
+    auto temp_current_function = std::to_underlying(current_function);
+    ImGui::Combo("Function", &temp_current_function, function_names.data(), function_names.size());
+    current_function = static_cast<Function>(temp_current_function);
     ImGui::SliderInt("N", &N, 0, 100);
     ImGui::SliderFloat("f", &speed, 0, 0.15);
+    ImGui::SliderFloat("Radius", &constant_radius, 0.1f, 200.f);
     ImGui::End();
 
     // ImGui::ShowDemoWindow();
@@ -63,15 +73,14 @@ void Update()
     auto position = raylib::Vector2{0, 0};
     for (auto i = 0; i < N; ++i)
     {
-        const auto n = i * 2 + 1;
-        const auto previous = position;
-        const auto radius = 75 * (4 / (n * math::pi_v<float>));
+        const auto [n, radius] = GetFormula(i);
+        raylib::DrawCircleLinesV(translate + position, radius, raylib::Color{255, 255, 255, 100});
+        const auto previous_position = position;
         position += {
             radius * math::cos(n * t),
             radius * math::sin(n * t)
         };
-        raylib::DrawCircleLinesV(translate + previous, radius, raylib::Color{255, 255, 255, 100});
-        raylib::DrawLineV(translate + previous, translate + position, raylib::RAYWHITE);
+        raylib::DrawLineV(translate + previous_position, translate + position, raylib::RAYWHITE);
     }
 
     wave.push_front(position.y);
@@ -83,4 +92,44 @@ void Update()
 
     raylib::DrawLineStrip(points, raylib::WHITE);
     t += speed;
+}
+
+std::pair<int, float> GetFormula(const int i)
+{
+    switch (current_function)
+    {
+        using enum Function;
+        case SQUARE_WAVE:
+        {
+            const auto n = i * 2 + 1;
+            const auto coeff = 4.f / (n * math::pi_v<float>);
+            const auto radius = constant_radius * (coeff / (4.f / math::pi_v<float>));
+            // radius = constant_radius * (4 / (n * math::pi_v<float>));
+            return { n, radius };
+        }
+
+        case SAWTOOTH_WAVE:
+        {
+            const auto n = i + 1;
+            const auto coeff = 2.f / (n * math::pi_v<float>);
+            const auto radius = 0.5f - constant_radius * (coeff / (2.f / math::pi_v<float>));
+            // radius = constant_radius * (1 / (n * math::pi_v<float>));
+            return { n, radius };
+        }
+
+        case TRIANGLE_WAVE:
+        {
+            break;
+        }
+
+        case SEMICIRCLE:
+        {
+            break;
+        }
+
+        default:
+        break;
+    }
+
+    return {};
 }
