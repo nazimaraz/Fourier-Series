c5a60519c001832c012d99400b30b7bb852e2a24
nazimaraz <46851561+nazimaraz@users.noreply.github.com>
2026-06-28 20:12:14 +0300

    update

--- cmake/CompileOptions.cmake
+++ cmake/CompileOptions.cmake
@@ -1,23 +1,22 @@
-# Example: CompileOptions(app_name)
-function(CompileOptions app_name)
-    target_compile_options(${app_name} PRIVATE
-        # Enabled options
-        -Wall
-        -Wextra
-        -Wpedantic
-        -Werror
+include(CheckCXXCompilerFlag)
+function(CompileOptions target_name)
+    if (MSVC)
+        target_compile_options(${target_name} PRIVATE /W4)
+        return()
+    endif ()
 
-        # disabled options
-        -Wno-unused-function
-        -Wno-unused-lambda-capture
-        -Wno-unused-parameter
-        -Wno-unused-variable
-        -Wno-unused-private-field
-        -Wno-missing-field-initializers
-        -Wno-gnu-zero-variadic-macro-arguments
-        -Wno-invalid-noreturn
-        -Wno-unused-value
-        -Wno-unused-local-typedef
-        -Wno-unused-but-set-variable
+    set(options
+            -Wall
+            -Wextra
+            -Wpedantic
+            -Wno-missing-field-initializers
     )
+
+    foreach (option IN LISTS options)
+        string(MAKE_C_IDENTIFIER "has_cxx_flag_${option}" flag_variable)
+        check_cxx_compiler_flag("${option}" ${flag_variable})
+        if (${flag_variable})
+            target_compile_options(${target_name} PRIVATE "${option}")
+        endif ()
+    endforeach ()
 endfunction()
